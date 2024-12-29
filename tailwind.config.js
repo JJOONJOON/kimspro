a55ebf7da5fcfcab45905cd3a5ffb56d3fc36401
JOON <suitjoon@gmail.com>
2024-12-29 22:23:17 +0900

    Fix: Update project configuration for Vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
